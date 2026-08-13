64643e0a10792d309801dda7e1f1abb711a4154b
Amir-fan <amir.f.fanari@gmail.com>
2026-08-13 10:43:51 +0300

    Batch rename catalog images based on color categories

--- catalog-data.js
+++ catalog-data.js
@@ -1,152 +1,152 @@
 const catalogItems = [
     {
-        "src": "katalog/اكياس غير مطبوعة (11) black.png",
+        "src": "katalog/اكياس غير مطبوعة (11) black_black.png",
         "category": "اكياس غير مطبوعة",
         "details": "black"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (17) black.png",
+        "src": "katalog/اكياس غير مطبوعة (17) black_black.png",
         "category": "اكياس غير مطبوعة",
         "details": "black"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (19) black.png",
+        "src": "katalog/اكياس غير مطبوعة (19) black_black.png",
         "category": "اكياس غير مطبوعة",
         "details": "black"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (30) black.png",
+        "src": "katalog/اكياس غير مطبوعة (30) black_black.png",
         "category": "اكياس غير مطبوعة",
         "details": "black"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (31) black.png",
+        "src": "katalog/اكياس غير مطبوعة (31) black_black.png",
         "category": "اكياس غير مطبوعة",
         "details": "black"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (6) brown.png",
+        "src": "katalog/اكياس غير مطبوعة (6) brown_brown.png",
         "category": "اكياس غير مطبوعة",
         "details": "brown"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (7) brown.png",
+        "src": "katalog/اكياس غير مطبوعة (7) brown_brown.png",
         "category": "اكياس غير مطبوعة",
         "details": "brown"
     },
 
     {
-        "src": "katalog/اكياس غير مطبوعة (31) gold.png",
+        "src": "katalog/اكياس غير مطبوعة (31) gold_gold.png",
         "category": "اكياس غير مطبوعة",
         "details": "gold"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (34) gold.png",
+        "src": "katalog/اكياس غير مطبوعة (34) gold_gold.png",
         "category": "اكياس غير مطبوعة",
         "details": "gold"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (35) gold.png",
+        "src": "katalog/اكياس غير مطبوعة (35) gold_gold.png",
         "category": "اكياس غير مطبوعة",
         "details": "gold"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (36) gold.png",
+        "src": "katalog/اكياس غير مطبوعة (36) gold_gold.png",
         "category": "اكياس غير مطبوعة",
         "details": "gold"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (37) gold.png",
+        "src": "katalog/اكياس غير مطبوعة (37) gold_gold.png",
         "category": "اكياس غير مطبوعة",
         "details": "gold"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (32) white silver.png",
+        "src": "katalog/اكياس غير مطبوعة (32) white silver_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "white silver"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (33) white silver.png",
+        "src": "katalog/اكياس غير مطبوعة (33) white silver_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "white silver"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (34) white silver.png",
+        "src": "katalog/اكياس غير مطبوعة (34) white silver_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "white silver"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (35) white silver.png",
+        "src": "katalog/اكياس غير مطبوعة (35) white silver_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "white silver"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (36) white silver.png",
+        "src": "katalog/اكياس غير مطبوعة (36) white silver_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "white silver"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (37) white silver.png",
+        "src": "katalog/اكياس غير مطبوعة (37) white silver_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "white silver"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (38) white silver.png",
+        "src": "katalog/اكياس غير مطبوعة (38) white silver_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "white silver"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (39) white silver.png",
+        "src": "katalog/اكياس غير مطبوعة (39) white silver_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "white silver"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (40) white silver.png",
+        "src": "katalog/اكياس غير مطبوعة (40) white silver_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "white silver"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (41) white silver.png",
+        "src": "katalog/اكياس غير مطبوعة (41) white silver_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "white silver"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (42) white silver.png",
+        "src": "katalog/اكياس غير مطبوعة (42) white silver_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "white silver"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (32) see through.png",
+        "src": "katalog/اكياس غير مطبوعة (32) see through_white_silver_cream.png",
         "category": "اكياس غير مطبوعة",
         "details": "see through"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (33) see through.png",
+        "src": "katalog/اكياس غير مطبوعة (33) see through_transparent.png",
         "category": "اكياس غير مطبوعة",
         "details": "see through"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (36) see through.png",
+        "src": "katalog/اكياس غير مطبوعة (36) see through_transparent.png",
         "category": "اكياس غير مطبوعة",
         "details": "see through"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (37) see through.png",
+        "src": "katalog/اكياس غير مطبوعة (37) see through_transparent.png",
         "category": "اكياس غير مطبوعة",
         "details": "see through"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (38) see through.png",
+        "src": "katalog/اكياس غير مطبوعة (38) see through_transparent.png",
         "category": "اكياس غير مطبوعة",
         "details": "see through"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (39) see through.png",
+        "src": "katalog/اكياس غير مطبوعة (39) see through_transparent.png",
         "category": "اكياس غير مطبوعة",
         "details": "see through"
     },
     {
-        "src": "katalog/اكياس غير مطبوعة (10).jpeg",
+        "src": "katalog/اكياس غير مطبوعة (10)_standard.jpeg",
         "category": "اكياس غير مطبوعة",
         "details": "standard"
     }
--- catalog2-data.js
+++ catalog2-data.js
@@ -1,231 +1,231 @@
 const catalogItems2 = [
     {
-        "src": "katalog2/اكياس غير مطبوعة (8) brown.jpeg",
+        "src": "katalog2/اكياس غير مطبوعة (8) brown_brown.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (1).jpeg",
+        "src": "katalog2/اكياس مطبوعة (1)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (22).jpeg",
+        "src": "katalog2/اكياس مطبوعة (22)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (23).jpeg",
+        "src": "katalog2/اكياس مطبوعة (23)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (24).jpeg",
+        "src": "katalog2/اكياس مطبوعة (24)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (25).jpeg",
+        "src": "katalog2/اكياس مطبوعة (25)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (26).jpeg",
+        "src": "katalog2/اكياس مطبوعة (26)_brown.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (27).jpeg",
+        "src": "katalog2/اكياس مطبوعة (27)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (28).jpeg",
+        "src": "katalog2/اكياس مطبوعة (28)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (29).jpeg",
+        "src": "katalog2/اكياس مطبوعة (29)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (30).jpeg",
+        "src": "katalog2/اكياس مطبوعة (30)_brown.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (31).jpeg",
+        "src": "katalog2/اكياس مطبوعة (31)_brown.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (32).jpeg",
+        "src": "katalog2/اكياس مطبوعة (32)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (33).jpeg",
+        "src": "katalog2/اكياس مطبوعة (33)_brown.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (34).jpeg",
+        "src": "katalog2/اكياس مطبوعة (34)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (35).jpeg",
+        "src": "katalog2/اكياس مطبوعة (35)_brown.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (36).jpeg",
+        "src": "katalog2/اكياس مطبوعة (36)_brown.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (37).jpeg",
+        "src": "katalog2/اكياس مطبوعة (37)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (38).jpeg",
+        "src": "katalog2/اكياس مطبوعة (38)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (39).jpeg",
+        "src": "katalog2/اكياس مطبوعة (39)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (40).jpeg",
+        "src": "katalog2/اكياس مطبوعة (40)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (41).jpeg",
+        "src": "katalog2/اكياس مطبوعة (41)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (42).jpeg",
+        "src": "katalog2/اكياس مطبوعة (42)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (43).jpeg",
+        "src": "katalog2/اكياس مطبوعة (43)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (44).jpeg",
+        "src": "katalog2/اكياس مطبوعة (44)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (45).jpeg",
+        "src": "katalog2/اكياس مطبوعة (45)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (46).jpeg",
+        "src": "katalog2/اكياس مطبوعة (46)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (47).jpeg",
+        "src": "katalog2/اكياس مطبوعة (47)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (48).jpeg",
+        "src": "katalog2/اكياس مطبوعة (48)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (49).jpeg",
+        "src": "katalog2/اكياس مطبوعة (49)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (50).jpeg",
+        "src": "katalog2/اكياس مطبوعة (50)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (51).jpeg",
+        "src": "katalog2/اكياس مطبوعة (51)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (52).jpeg",
+        "src": "katalog2/اكياس مطبوعة (52)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (53).jpeg",
+        "src": "katalog2/اكياس مطبوعة (53)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (54).jpeg",
+        "src": "katalog2/اكياس مطبوعة (54)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (55).jpeg",
+        "src": "katalog2/اكياس مطبوعة (55)_brown.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (56).jpeg",
+        "src": "katalog2/اكياس مطبوعة (56)_brown.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (57).jpeg",
+        "src": "katalog2/اكياس مطبوعة (57)_black.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (58).jpeg",
+        "src": "katalog2/اكياس مطبوعة (58)_black.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (59).jpeg",
+        "src": "katalog2/اكياس مطبوعة (59)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (60).jpeg",
+        "src": "katalog2/اكياس مطبوعة (60)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (61).jpeg",
+        "src": "katalog2/اكياس مطبوعة (61)_black.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (62).jpeg",
+        "src": "katalog2/اكياس مطبوعة (62)_black.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (63).jpeg",
+        "src": "katalog2/اكياس مطبوعة (63)_black.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (64).jpeg",
+        "src": "katalog2/اكياس مطبوعة (64)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     },
     {
-        "src": "katalog2/اكياس مطبوعة (65).jpeg",
+        "src": "katalog2/اكياس مطبوعة (65)_white_silver_cream.jpeg",
         "category": "اكياس مطبوعة",
         "details": "printed"
     }
